64e6de7277b232419aa3873f6dea3d66865347a3
akang297 <94566176+akang297@users.noreply.github.com>
2023-09-21 08:32:00 -0500

    Made key

---
+++ main.py
@@ -0,0 +1,55 @@
+import winsound
+
+fre=0
+
+
+def c (fre):
+    fre=523
+    return fre
+def d (fre):
+    fre=587
+    return fre
+def e (fre):
+    fre=659
+    return fre
+def f (fre):
+    fre=698
+    return fre
+def g (fre):
+    fre=784
+    return fre
+def a (fre):
+    fre=880
+    return fre
+def b (fre):
+    fre=987
+    return fre
+
+
+    
+
+winsound.Beep(g(fre), 500)
+winsound.Beep(e(fre), 500)
+winsound.Beep(f(fre), 500)
+winsound.Beep(g(fre), 500)
+winsound.Beep(e(fre), 500)
+winsound.Beep(f(fre), 500)
+winsound.Beep(g(fre), 500)
+winsound.Beep(g(fre)/2, 500)
+winsound.Beep(a(fre)/2, 500)
+winsound.Beep(b(fre)/2, 500)
+winsound.Beep(c(fre), 500)
+winsound.Beep(d(fre), 500)
+winsound.Beep(e(fre), 500)
+winsound.Beep(f(fre), 500)
+winsound.Beep(e(fre), 500)
+winsound.Beep(c(fre), 500)
+winsound.Beep(d(fre), 500)
+winsound.Beep(e(fre), 500)
+winsound.Beep(e(fre)/2, 500)
+winsound.Beep(f(fre)/2, 500)
+winsound.Beep(g(fre)/2, 500)
+winsound.Beep(a(fre)/2, 500)
+winsound.Beep(g(fre)/2, 500)
+winsound.Beep(f(fre)/2, 500)
+winsound.Beep(g(fre)/2, 500)
